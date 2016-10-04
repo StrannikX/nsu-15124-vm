971def6043b082291702a88721fa19b6aad6ca24
Danila Kalinin <strannik.danil@gmail.com>
2016-10-04 20:20:58 +0700

    Закончен алгоритм поиска решения СЛАУ методом прогонки

--- progon.cpp
+++ progon.cpp
@@ -3,6 +3,21 @@
 # include <cmath>
 # include <vector>
 
+/*
+*   Будем считать, что матрица имеет вид
+*  --------------------------------------
+*  | -b1  g1  0   0  ...  0   0  |  d1  |
+*  |  a2 -b2  g2  0  ...  0   0  |  d2  |
+*  |  0   a3 -b3  g3 ...  0   0  |  d3  |
+*  | ... ... ... ... ... ... ... | ...  |
+*  |  0   0  ... ...  an -bn  gn |  dn  |
+*  --------------------------------------
+*/
+
+# define A(m, n) m[n][n-1]  // an
+# define B(m, n) (-m[n][n]) // bn
+# define G(m, n) m[n][n+1]  // gn
+
 using namespace std;
 
 int main(int argc, char**) {
@@ -15,10 +30,10 @@ int main(int argc, char**) {
     // Сама матрица
     vector<vector<double> > a(n);
     // Вектор значений ( b в Ax=b)
-    vector<double> vec(n);
+    vector<double> d(n), x(n);
 
     // Вектор для значений P
-    std::vector<double> p(n - 1);
+    std::vector<double> p(n - 1), q(n - 1);
 
     // Считываем матрицу A
     for (int i = 0; i < n; i++) {
@@ -31,14 +46,42 @@ int main(int argc, char**) {
 
     cout << endl;
 
-    // Считываем вектор значений b
+    // Считываем вектор значений d
     for (int i = 0; i < n; i++) {
-        cin >> vec[i];
+        cin >> d[i];
     }
 
-    p[0] = - a[0][1] / a[0][0];
+    /*
+    *  Приводим матрицу к виду
+    *  --------------------------------------
+    *  |  1 -p1   0   0  ...  0   0  |  q1  |
+    *  |  0  1  -p2   0  ...  0   0  |  q2  |
+    *  |  0  0    1  -p3 ...  0   0  |  q3  |
+    *  | ... ... ... ... ... ... ... | ...  |
+    *  |  0   0   0   0  ...  0   1  |  qn  |
+    *  --------------------------------------
+    */
+
+    p[0] = G(a, 0) / B(a, 0);
+    q[0] = - b[0] / B(a, 0);
     for (int i = 1; i < n - 1; i++) {
-        p[i] = a[i][i + 1] / 
+        p[i] = G(a, i) / (B(a, i) - A(a, i) * p[i - 1]);
+        q[i] = (A(a, i) * q[i - 1] - d[i]) / (B(a, i) - A(a, i) * p[i - 1]);
+    }
+
+    /**
+    * Обратный ход алгоритм, поиск решения системы
+    */
+
+    x[n - 1] = (A(a, n - 1) * q[n - 2] - d[n - 1]) / (B(a, n - 1) - A(a, n - 1) * p[n - 2]);
+    for (int i = n - 2; i >= 0; i--) {
+        x[i] = p[i] * x[i + 1] + q[i];
+    }
+
+
+    cout.setf(ios_base::fixed);
+    for (int i = 0; i < n; i++) {
+        cout << "x" << i + 1 << " = " << x[i] << endl;
     }
 
     return 0;
